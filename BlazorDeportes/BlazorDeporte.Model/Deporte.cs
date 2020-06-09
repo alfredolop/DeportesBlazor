e01d1ab3714c778b655bea4ad6f4cb3b7c391857
alfredolop <alfredolop710@gmail.com>
2020-06-09 19:55:41 +0200

    agrego la clase Deporte y hago una referencia hacia BlazorDeportes

---
+++ BlazorDeportes/BlazorDeporte.Model/Deporte.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDeporte.Model
+{
+    public class Deporte
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int NumJugadores { get; set; }
+    }
+}
