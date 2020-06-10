fd2c84c3e26a8aa891cb51a31f7d064984971493
alfredolop <alfredolop710@gmail.com>
2020-06-10 20:01:09 +0200

    Dentro de la carpeta Interfaces creo una interfaz y dentro de la carpeta Services creo una clase

---
+++ BlazorDeportes/BlazorDeportes/Interfaces/IDeporteService.cs
@@ -0,0 +1,16 @@
+﻿using BlazorDeportes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDeportes.Interfaces
+{
+    public interface IDeporteService
+    {
+        Task<IEnumerable<Deporte>> GetAllDeportes();
+        Task<Deporte> GetDetails(int id);
+        Task<bool> DeleteDeporte(int id);
+        Task<bool> SaveDeporte(Deporte deporte);
+    }
+}
---
+++ BlazorDeportes/BlazorDeportes/Services/DeporteService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorDeportes.Interfaces;
+using BlazorDeportes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDeportes.Services
+{
+    public class DeporteService : IDeporteService
+    {
+        public Task<IEnumerable<Deporte>> GetAllDeportes()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Deporte> GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> DeleteDeporte(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> SaveDeporte(Deporte deporte)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
